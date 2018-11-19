f73fc02e501ba8d786ba09cd6707bcdb97a184e6
JOHN D <johndbvb@hotmail.com>
2018-11-19 11:56:28 -0500

    subir archivo

---
+++ src/sumanum.java
@@ -0,0 +1,13 @@
+
+public class sumanum {
+
+	public static void main(String[] args) {
+		int num1 =2;
+		int num2 =3;
+		int rel = num1+num2;
+		
+		System.out.println("el resultado es :"+ rel);
+
+	}
+
+}
